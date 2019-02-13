93cbbcd32f8f29f75fa8af8417debce1448b2d32
FrankZappaLevasseur <francois_levasseur@hotmail.com>
2019-02-13 15:31:34 -0500

    separate predicitions for casual and registered

--- src/predict_main.py
+++ src/predict_main.py
@@ -1,7 +1,6 @@
 import numpy as np
 from typing import List
 from matplotlib import pyplot
-import math
 
 from sklearn.model_selection import train_test_split
 from sklearn.svm import LinearSVR
@@ -12,9 +11,16 @@ from data import Data
 from constants import *
 
 
-def format_data_for_prediction(d: Data):
-    return (d.temperature,
+def format_data_for_casual_prediction(d: Data):
+    return (d.date.hour,
+            d.temperature,
             d.working_day,
+            d.humidity)
+
+
+def format_data_for_registered_prediction(d: Data):
+    return (d.date.hour,
+            d.temperature,
             d.humidity)
 
 
@@ -50,22 +56,31 @@ if __name__ == "__main__":
     pass
 
     # ----------------- Training and prediction -----------------
-    y = np.array([d.total_cnt for d in whole_data])
-    X = np.array([format_data_for_prediction(d) for d in whole_data])
+    y1 = np.array([d.casual_cnt for d in whole_data])
+    y2 = np.array([d.registered_cnt for d in whole_data])
+    X1 = np.array([format_data_for_casual_prediction(d) for d in whole_data])
+    X2 = np.array([format_data_for_registered_prediction(d) for d in whole_data])
 
     data_size = len(whole_data)
     train_sample_size = data_size * 2 // 3
 
     train_indexes, test_indexes = train_test_split(np.arange(data_size), train_size=train_sample_size)
-    X_train = X[train_indexes]
-    X_test = X[test_indexes]
-    y_train = y[train_indexes]
-    y_test = y[test_indexes]
+    X1_train = X1[train_indexes]
+    X1_test = X1[test_indexes]
+    X2_train = X2[train_indexes]
+    X2_test = X2[test_indexes]
 
-    model = RandomForestRegressor()
-    model.fit(X_train, y_train)
+    y1_train = y1[train_indexes]
+    y2_train = y2[train_indexes]
+    y_test = y1[test_indexes] + y2[test_indexes]
 
-    y_hat = model.predict(X_test)
+    model1 = RandomForestRegressor()
+    model1.fit(X1_train, y1_train)
+
+    model2 = RandomForestRegressor()
+    model2.fit(X2_train, y2_train)
+
+    y_hat = model1.predict(X1_test) + model2.predict(X2_test)
 
     squared_deviations_sum = (np.square(y_hat - y_test)).sum()
     print("La somme des résidus carrés est : {0:,.2f}".format(squared_deviations_sum))
@@ -82,9 +97,14 @@ if __name__ == "__main__":
     pyplot.show()
 
     # ----------------- validation -----------------
+    model1.fit(X1, y1)  # on a gaspillé le 1/3 du dataset tentot pour l'entrainement alors ici on le reprend au complet
+    model2.fit(X2, y2)
+
     test_data: List[Data] = data_repository.read_test_data(TEST_FILE_PATH)
-    X_submission = np.array([format_data_for_prediction(d) for d in test_data])
-    y_submission = model.predict(X_submission)
+    X1_submission = np.array([format_data_for_casual_prediction(d) for d in test_data])
+    X2_submission = np.array([format_data_for_registered_prediction(d) for d in test_data])
+
+    y_submission = model1.predict(X1_submission) + model2.predict(X2_submission)
 
     data_repository.write_submission_data(SUBMISSION_TEMPLATE_FILE_PATH, CURRENT_SUBMISSION, y_submission)
 
